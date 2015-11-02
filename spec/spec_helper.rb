6afc83317e437da5d25a94639f149f87e41dde77
lucerion <alexander.skachko@gmail.com>
2015-11-03 01:01:36 +0300

    Fix code climate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'jsregexp'
 require 'minitest/autorun'
 require 'codeclimate-test-reporter'
 
 CodeClimate::TestReporter.start
+
+require 'jsregexp'
