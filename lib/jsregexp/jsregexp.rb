1c46d3201f75ebc0645c50360008e47b57b84442
a.skachko <a.skachko@itransition.com>
2016-02-02 19:49:58 +0300

    convert method refactoring

--- lib/jsregexp/jsregexp.rb
+++ lib/jsregexp/jsregexp.rb
@@ -4,20 +4,27 @@ class JSRegexp
   #
   # @params regexp [Regexp, String] a Ruby regular expression
   # @return [String] JavaSript regular expression represented as a string
-  def self.convert(regexp)
-    regexp_str = regexp.is_a?(Regexp) ? regexp.inspect : regexp.to_s
-    regexp_str
-      .sub(/^\// , '')
-      .sub(/\/[a-z]*$/ , '')
-      .sub('\\A', '^')
-      .sub('\\G', '^')
-      .sub('\\Z', '$')
-      .sub('\\z', '$')
-      .gsub('\\R', '')
-      .gsub('\\a', '')
-      .gsub('\\e', '')
-      .gsub(/\(\?#.+\)/, '')
-      .gsub(/\(\?-\w+:/, '(')
-      .gsub(/\s/, '')
+  class << self
+    def convert(regexp)
+      stringify(regexp)
+        .sub(/^\//, '')
+        .sub(/\/[a-z]*$/, '')
+        .sub('\\A', '^')
+        .sub('\\G', '^')
+        .sub('\\Z', '$')
+        .sub('\\z', '$')
+        .gsub('\\R', '')
+        .gsub('\\a', '')
+        .gsub('\\e', '')
+        .gsub(/\(\?#.+\)/, '')
+        .gsub(/\(\?-\w+:/, '(')
+        .gsub(/\s/, '')
+    end
+
+    private
+
+    def stringify(regexp) # :nodoc:
+      regexp.is_a?(Regexp) ? regexp.inspect : regexp.to_s
+    end
   end
 end
